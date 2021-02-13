373116387008e93f856557645872b5860914359e
Ayhan Özer <Ayhan zer@DESKTOP-HTDLLF0>
2021-02-13 23:55:48 +0300

    13 Şubat ders sonu

--- MyFinalProject/Business/Concrete/ProductManager.cs
+++ MyFinalProject/Business/Concrete/ProductManager.cs
@@ -24,7 +24,7 @@ namespace Business.Concrete
         {
             //İş Kodları
             //Yetkisi var mı?
-            if (DateTime.Now.Hour==22)
+            if (DateTime.Now.Hour == 22)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
